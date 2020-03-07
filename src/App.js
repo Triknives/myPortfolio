b3451fe1aabc8b5798f20af4365af4a26d3a5ff9
Triknives <cordell.desu@gmail.com>
2020-03-07 13:47:11 -0800

    create structure and nav for portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './components/Projects';
+import Articles from './components/Articles';
+import About from './components/About';
+import AppNavbar from './components/AppNavbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavbar/>
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
+// <BrowserRouter>
+// <div className="App">
+//
+// <Route exact path="/" component={Projects} />
+// <Route path="/articles" component={Articles} />
+// <Route path="/about" component={About} />
+//
+// <div className="navigation">
+// <div className="navigation-sub">
+//
+// <Link to="/" className="item">Projects</Link>
+// <Link to="/articles" className="item">Articles</Link>
+// <Link to="/about" className="item">About</Link>
+//
+// </div>
+// </div>
+// </div>
+// </BrowserRouter>
