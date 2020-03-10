6275010fd2ff006d9f04f45ee7b7271ed336676a
Triknives <50305423+Triknives@users.noreply.github.com>
2020-03-10 15:50:26 -0700

    update content, and styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,7 @@ function About(props) {
     <Container>
       <div className="about-me">
       <h1>Hello, my name is Brett Cordell!</h1>
-      <p>I am a Full-Stack developer, with experiencing creating applications with React.js and Ruby On Rails. Currently working with "MERN" stack. I also love learning new frameworks, languages and design strategies.
-      </p>
+      <p>I am a Full-Stack developer, with experiencing creating applications with React.js and Ruby On Rails. Currently working with "MERN" stack. I also love learning new frameworks, languages and design strategies.</p>
     </div>
     </Container>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -17,8 +17,8 @@ function Projects(props) {
       <Card>
         <CardBody>
         <CardTitle><h2>DingBat</h2></CardTitle>
+        <CardSubtitle><strong>Ruby on Rails | Action Cable | PostGreSql</strong></CardSubtitle>
         <CardImg top width="100%" img src={dingBatCard} alt="Card image cap" />
-          <CardSubtitle><strong>Ruby on Rails | Action Cable | PostGreSql</strong></CardSubtitle>
           <CardText>A browser based party trivia game based off the "JackBox" series on Nintendo Switch. Players can sign in to a game via mobile devices. Host device is ideally a monitor or T.V.</CardText>
           <Button href="http://dingbat-trivia.herokuapp.com">Visit Page</Button>
         </CardBody>
@@ -30,9 +30,9 @@ function Projects(props) {
         <Card>
           <CardBody>
           <CardTitle><h2>Marios Reviews</h2></CardTitle>
+          <CardSubtitle><strong>Ruby on Rails | PostgreSql | Auth.</strong></CardSubtitle>
           <CardImg top width="100%" img src={mariosReview1} alt="Card image cap" />
-            <CardSubtitle><strong>Ruby on Rails | PostgreSql | Auth.</strong></CardSubtitle>
-            <CardText>A browser based party trivia game based off the "JackBox" series on Nintendo Switch. Players can sign in to a game via mobile devices. Hot.</CardText>
+            <CardText>A full stack application, mock up site with authorizations through Devise. Users can sign up, and add a product to the site and leave a review.</CardText>
             <Button href="https://fierce-lake-38166.herokuapp.com/users/sign_in">Visit Page</Button>
           </CardBody>
         </Card>
@@ -43,8 +43,8 @@ function Projects(props) {
           <Card>
             <CardBody>
             <CardTitle><h2>Toshou</h2></CardTitle>
+            <CardSubtitle><strong>React.JS | MERN Stack | Redux</strong></CardSubtitle>
             <CardImg top width="100%" img src={toshou} alt="Card image cap" />
-              <CardSubtitle><strong>React.Js | MERN Stack | Redux</strong></CardSubtitle>
               <CardText>A browser based party trivia game based off the "JackBox" series on Nintendo Switch. Players can sign in to a game via mobass</CardText>
               <Button>Visit Page</Button>
             </CardBody>
